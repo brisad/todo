30a369cd53ebbd8ae545edf68a96c7395b9cbacd
Michael Brennan <brennan.brisad@gmail.com>
2014-10-27 16:57:48 +0100

    Open and close db before and after each request
    Do it the way explained in the official Flask tutorial.

--- app.py
+++ app.py
@@ -10,6 +10,16 @@ app.config.from_object(__name__)
 def connect_db():
     return MongoClient(app.config['DATABASE'])
 
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
 @app.route('/')
 def root():
     return render_template('index.html')
