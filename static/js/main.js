39a3f142281aad02ed63c6058fd81897f6b79342
Michael Brennan <brennan.brisad@gmail.com>
2014-10-31 17:07:08 +0100

    Enable login when pressing enter key

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,14 @@
 
 $('#login').on("click", function () {
-    $('#login-form').submit();
+  $('#login-form').submit();
 });
 
 $('#loginModal').on('shown.bs.modal', function () {
-    $('#username').focus();
+  $('#username').focus();
 })
+
+$('#loginModal form').keypress(function (e) {
+  if (e.which == 13) {
+    $('#login-form').submit();
+  }
+});
